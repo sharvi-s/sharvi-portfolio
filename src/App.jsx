2881cefbaa7c7e4c7921e4882db760b50eb29102
Sharvi Sriperambudur <sharvisp2@gmail.com>
2026-05-16 15:27:45 -0500

    set up the react vite portfolio shell

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <iframe
+      className="portfolio-frame"
+      src="/sharvi_portfolio.html"
+      title="Sharvi Sriperambudur Portfolio"
+    />
+  );
+}
